f60ea00ce86a5cb3c45bc7426ad42d59e8c20d08
info <info@resilience-theatre.com>
2023-07-15 05:17:49 +0300

    New branch for tunneled wireguard development.
    Note that these changes requires updated keygeneration environment
    and gw capable to terminate these connections.

--- board/pinephone/pine/usr/bin/firstboot.sh
+++ board/pinephone/pine/usr/bin/firstboot.sh
@@ -13,6 +13,9 @@ else
     	echo "This is first boot!"
     	cp /opt/tunnel/network-configurations/wg0.* /etc/systemd/network/
     	cp /opt/tunnel/service-files/* /etc/systemd/system/
+	mkdir /opt/wgcap
+	cp /opt/tunnel/wgcap_service.conf /opt/wgcap
+	ln -s /etc/systemd/system/rtptun.service /etc/systemd/system/multi-user.target.wants/
 	# ringtone
 	cp /etc/ringtone.wav /opt/tunnel/ringtone.wav
     	touch /etc/fbc
